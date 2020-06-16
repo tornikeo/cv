90c0e0bc81b78cc471368dcf25edeaba8085d02d
tornikeo <tonop15@freeuni.edu.ge>
2020-06-16 16:21:01 +0400

    Added test for layout and styling

--- cv/functional_tests/base.py
+++ cv/functional_tests/base.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
+
+MAX_WAIT = 5
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         options = webdriver.firefox.options.Options()
-        # options.headless = True
+        options.headless = True
         self.browser = webdriver.Firefox(options=options)
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for(self, assertion):
+        start_time = time.time()
+        while True:
+            try:
+                return assertion()
+            except (AssertionError,WebDriverException)  as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(.5)
--- cv/functional_tests/test_layout_and_styling.py
+++ cv/functional_tests/test_layout_and_styling.py
@@ -7,17 +7,30 @@ class LayoutAndStylingTest(FunctionalTest):
         # She goes to check out his homepage.
         # self.browser.get(self.live_server_url)
         self.browser.get('http://localhost:8000')
+        self.browser.set_window_size(1024, 768)
 
         # She notices the name of the develoepr on page title
         self.assertIn('Tornike', self.browser.title)
 
         # She sees a picture of the dev
+        self.wait_for(lambda: self.browser.find_element_by_id("id_picture"))
 
         # She sees the name "TornikeO"
+        header_text = self.wait_for(lambda: 
+            self.browser.find_element_by_tag_name("h1")
+        )
+        self.assertIn('TornikeO', header_text)
 
         # There is email link. The email link redirects
         # To email service. email is "tonop15@freeuni.edu.ge"
-
+        email_link_tag = self.wait_for(
+            lambda: self.browser.find_elements_by_id('id_email')
+        )
+        self.assertIn(
+            "tonop15@freeuni.edu.ge",
+            email_link_tag.get_attribute("href")
+        )
+        self.fail("Finish test!")
         # She closes the email tab and returns to
         # scroll down more.
 
